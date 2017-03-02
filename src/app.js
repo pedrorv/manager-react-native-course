3d7a7bfc969cd335ffa9bb115b652fe1017eb7e3
Pedro Reis <pedroreisv@gmail.com>
2017-03-01 21:20:26 -0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import firebase from 'firebase'
+
+import reducers from './reducers'
+
+class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyBDqbqcd59HDBIpbqkFvlrO_RbLN8q9www',
+      authDomain: 'manager-927ed.firebaseapp.com',
+      databaseURL: 'https://manager-927ed.firebaseio.com',
+      storageBucket: 'manager-927ed.appspot.com',
+      messagingSenderId: '346567615156'
+    }
+    firebase.initializeApp(config)
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>Hello</Text>
+        </View>
+      </Provider>
+    )
+  }
+}
+
+export default App
