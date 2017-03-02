ff8bd06396507f02533c7f13c0f50f8340ae7aaf
Pedro Reis <pedroreisv@gmail.com>
2017-03-02 14:56:15 -0300

    Add Confirm component. Add delete employee functionality

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -3,12 +3,14 @@ import { connect } from 'react-redux'
 import _ from 'lodash'
 import Communications from 'react-native-communications'
 
-import { employeeUpdate, employeeSave } from '../actions'
-import { Card, CardSection, Button } from './common'
+import { employeeUpdate, employeeSave, employeeDelete } from '../actions'
+import { Card, CardSection, Button, Confirm } from './common'
 import EmployeeForm from './EmployeeForm'
 
 
 class EmployeeEdit extends Component {
+  state = { showModal: false }
+
   componentWillMount() {
     _.each(this.props.employee, (value, prop) => {
       this.props.employeeUpdate({ prop, value })
@@ -27,6 +29,16 @@ class EmployeeEdit extends Component {
     Communications.text(phone, `Your upcoming shift is on ${shift}.`)
   }
 
+  onAccept() {
+    const { uid } = this.props.employee
+
+    this.props.employeeDelete({ uid })
+  }
+
+  onDecline() {
+    this.setState({ showModal: false })
+  }
+
   render() {
     return (
       <Card>
@@ -43,6 +55,20 @@ class EmployeeEdit extends Component {
             Text Schedule
           </Button>
         </CardSection>
+
+        <CardSection>
+          <Button onPress={() => this.setState({ showModal: !this.state.showModal })}>
+            Fire Employee
+          </Button>
+        </CardSection>
+
+        <Confirm
+          visible={this.state.showModal}
+          onAccept={this.onAccept.bind(this)}
+          onDecline={this.onDecline.bind(this)}
+        >
+          Are you sure you want to delete this?
+        </Confirm>
       </Card>
     )
   }
@@ -55,4 +81,11 @@ const mapStateToProps = (state) => {
   return { name, phone, shift }
 }
 
-export default connect(mapStateToProps, { employeeUpdate, employeeSave })(EmployeeEdit)
+export default connect(
+  mapStateToProps, 
+  { 
+    employeeUpdate, 
+    employeeSave, 
+    employeeDelete 
+  }
+)(EmployeeEdit)
