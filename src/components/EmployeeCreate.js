0f7fb04e6b91c135a2f381bd180b2c9e63ec9305
Pedro Reis <pedroreisv@gmail.com>
2017-03-02 13:03:49 -0300

    Add EmployeeForm component and refactor EmployeeCreate component

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -4,7 +4,7 @@ import { Picker, Text } from 'react-native'
 
 import { employeeUpdate, employeeCreate } from '../actions'
 import { Card, CardSection, Input, Button } from './common'
-
+import EmployeeForm from './EmployeeForm'
 
 
 class EmployeeCreate extends Component {
@@ -20,40 +20,7 @@ class EmployeeCreate extends Component {
 
     return (
       <Card>
-        <CardSection>
-          <Input 
-            label="Name"
-            placeholder="John Doe"
-            value={name}
-            onChangeText={value => employeeUpdate({ prop: 'name', value })}
-          />
-        </CardSection>
-
-        <CardSection>
-          <Input 
-            label="Phone"
-            placeholder="555-555-5555"
-            value={phone}
-            onChangeText={value => employeeUpdate({ prop: 'phone', value })}
-          />
-        </CardSection>
-
-        <CardSection style={{ flexDirection: 'column' }}>
-          <Text style={styles.pickerTextStyle}>Shift</Text>
-          <Picker
-            style={styles.pickerStyle}
-            selectedValue={this.props.shift}
-            onValueChange={value => this.props.employeeUpdate({ prop: 'shift', value })}
-          >
-            <Picker.Item label="Monday" value="Monday" />
-            <Picker.Item label="Tuesday" value="Tuesday" />
-            <Picker.Item label="Wednesday" value="Wednesday" />
-            <Picker.Item label="Thursday" value="Thursday" />
-            <Picker.Item label="Friday" value="Friday" />
-            <Picker.Item label="Saturday" value="Saturday" />
-            <Picker.Item label="Sunday" value="Sunday" />
-          </Picker>
-        </CardSection>
+        <EmployeeForm {...this.props} />
 
         <CardSection>
           <Button onPress={this.onButtonPress.bind(this)}>
@@ -65,17 +32,6 @@ class EmployeeCreate extends Component {
   }
 }
 
-const styles = {
-  pickerTextStyle: {
-    fontSize: 18,
-    paddingLeft: 20,
-    paddingTop: 5,
-  },
-  pickerStyle: {
-    marginLeft: 20,
-    marginRight: 20
-  }
-}
 
 const mapStateToProps = (state) => {
   const { name, phone, shift } = state.employeeForm
