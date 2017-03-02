c1a969ff0823df5536f5610e896e78df39bc5021
Pedro Reis <pedroreisv@gmail.com>
2017-03-02 11:09:30 -0300

    Add styles override to CardSection component. Add update sections functionality to EmployeeCreate component

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Picker, Text } from 'react-native'
+
+import { employeeUpdate } from '../actions'
 import { Card, CardSection, Input, Button } from './common'
 
 
 
 class EmployeeCreate extends Component {
   render() {
+    const { name, phone, employeeUpdate } = this.props
+
     return (
       <Card>
         <CardSection>
           <Input 
             label="Name"
             placeholder="John Doe"
+            value={name}
+            onChangeText={value => employeeUpdate({ prop: 'name', value })}
           />
         </CardSection>
 
@@ -18,11 +26,26 @@ class EmployeeCreate extends Component {
           <Input 
             label="Phone"
             placeholder="555-555-5555"
+            value={phone}
+            onChangeText={value => employeeUpdate({ prop: 'phone', value })}
           />
         </CardSection>
 
-        <CardSection>
-
+        <CardSection style={{ flexDirection: 'column' }}>
+          <Text style={styles.pickerTextStyle}>Shift</Text>
+          <Picker
+            style={styles.pickerStyle}
+            selectedValue={this.props.shift}
+            onValueChange={value => this.props.employeeUpdate({ prop: 'shift', value })}
+          >
+            <Picker.Item label="Monday" value="Monday" />
+            <Picker.Item label="Tuesday" value="Tuesday" />
+            <Picker.Item label="Wednesday" value="Wednesday" />
+            <Picker.Item label="Thursday" value="Thursday" />
+            <Picker.Item label="Friday" value="Friday" />
+            <Picker.Item label="Saturday" value="Saturday" />
+            <Picker.Item label="Sunday" value="Sunday" />
+          </Picker>
         </CardSection>
 
         <CardSection>
@@ -35,4 +58,21 @@ class EmployeeCreate extends Component {
   }
 }
 
-export default EmployeeCreate
+const styles = {
+  pickerTextStyle: {
+    fontSize: 18,
+    paddingLeft: 20,
+    paddingTop: 5,
+  },
+  pickerStyle: {
+    marginLeft: 20,
+    marginRight: 20
+  }
+}
+
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.employeeForm
+  return { name, phone, shift }
+}
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate)
